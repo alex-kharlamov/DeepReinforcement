3eefa180c8b0a98fd12177d00e76f3d3c6d21d42
Pavel Ostyakov <pavelosta@gmail.com>
2017-01-18 20:03:19 +0000

    Added prepare.sh

---
+++ prepare.sh
@@ -0,0 +1,10 @@
+apt-get update
+apt-get install cmake
+
+pip install --upgrade pip
+pip install gym
+pip install tensorflow
+pip install keras
+pip install gym[atari]
+pip install scikit-learn --upgrade
+pip install h5py
